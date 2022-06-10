ca60f35f16dc01fe2cedb68a009e320d4c479270
anderson gil <anderson.gil@karibu.cl>
2022-06-09 21:45:03 -0400

    boostrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <style>
         #sidebar-container {
             background: #1f1d2d;
@@ -87,7 +87,8 @@
 <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
 @yield('scripts')
-
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
 </html>
 
