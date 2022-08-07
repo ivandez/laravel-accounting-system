181141cbeb3c977e2fdf1dacfa91f21498017d6d
anderson gil <anderson.gil@karibu.cl>
2022-08-06 22:09:38 -0400

    chart gastos

--- resources/views/statistic/index.blade.php
+++ resources/views/statistic/index.blade.php
@@ -6,26 +6,46 @@
 
         <div class="row my-3 justify-content-center align-items-center">
             <div class="col">
-                <x-deudas-por-pagar :data="$deudasPorPagar"/>
-            </div>
-            <div class="col">
-                <x-deudas-por-cobrar :data="$deudasPorCobrar"/>
-            </div>
-        </div>
-
-        <div class="row mb-3">
-            <div class="col">
-                <x-most-seller-clients :clients="$clientesMasVentas"/>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col">
-                <x-most-seller-products :products="$products"/>
+                <canvas id="ventasYgastos"></canvas>
             </div>
         </div>
 
 
 
     </div>
+@endsection
+
+@section('scripts')
+<script src="{{ asset('js/app.js') }}"></script>
+<script>
+    // const labels = Utils.months({count: 7});
+    const data = {
+        labels: [1,2,3,4,5,6,7],
+        datasets: [
+            {
+            label: 'Ventas',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#97CC04',
+            tension: 0.1
+        },
+            {
+            label: 'Gastos',
+            data: [60,40,100,200,300,50,20],
+            fill: false,
+            borderColor: '#F04104',
+            tension: 0.1
+        },
+    ]
+    };
+    const config = {
+        type: 'line',
+        data: data,
+    };
+
+    const myChart = new Chart(
+    document.getElementById('ventasYgastos'),
+    config
+  );
+</script>
 @endsection
